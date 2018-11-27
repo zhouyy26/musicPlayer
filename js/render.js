82dbade1854fd820d7a4791de051da594b95dcf4
zhouyy26 <530357897@qq.com>
2018-11-27 16:13:20 +0800

    musicPlayer

---
+++ js/render.js
@@ -0,0 +1,43 @@
+// 渲染页面  图片 信息 播放列表
+(function ($, root) {
+    var $scope = $('.wrap');
+
+    // 图片
+    function renderImg(img) {
+        var oImg = new Image();
+        oImg.src = img;
+        oImg.onload = function () {
+            root.blurImg(oImg, $scope); // 高斯模糊背景图
+            $scope.find('.cover-img').html(oImg);
+        }
+    }
+
+    // 信息
+    function renderInfo(data) {
+        var dom = '<div class="song-name"><h1>' + data.song + '</h1></div>\
+                    <div class="singer-name"><span>' + data.singer + '</span></div>\
+                    <div class="album-name"><span>' + data.album + '</span></div>';
+        $scope.find('.song-info').html(dom);
+    }
+
+    // 渲染单曲信息
+    function renderSingleInfo(data) {
+        renderImg(data.image);
+        renderInfo(data);
+    }
+
+    // 渲染播放列表
+    function renderPlayList(data) {
+        var str = '';
+        data.forEach(function (ele) {
+            str += '<li><h2>' + ele.song + '</h2><span>-' + ele.singer + '</span></li>';
+        })
+        $scope.find('.play-cont ul').html(str);
+    }
+
+    // 暴露接口
+    root.render = {
+        singleInfo: renderSingleInfo,
+        playList: renderPlayList
+    };
+})(window.Zepto, window.player || (window.player = {}));
